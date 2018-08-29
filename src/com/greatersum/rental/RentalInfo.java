45be316a7daf1d6180263cf5941ab95b77d8dd30
Helena Strandberg <hejhej.helena@gmail.com>
2018-08-29 22:24:49 +0200

    Use Junit assertEquals instead of approvals for result comparison. Also use stringbuilder for "footer" lines as well. Get Movie vid method call instead of creating a new hashmap of the same movies for each rental. Move cost and points calculation into their own methods for readability and less cognitive complexity.

--- src/com/greatersum/rental/RentalInfo.java
+++ src/com/greatersum/rental/RentalInfo.java
@@ -5,60 +5,84 @@ import java.util.HashMap;
 import java.util.Objects;
 
 class RentalInfo {
+  private HashMap<String, Movie> movies = getMovies();
 
-    String statement(Customer customer) {
-        HashMap<String, Movie> movies = new HashMap<>();
-        movies.put("F001", new Movie("Ran", "regular"));
-        movies.put("F002", new Movie("Trois Couleurs: Bleu", "regular"));
-        movies.put("F003", new Movie("Cars 2", "childrens"));
-        movies.put("F004", new Movie("Latest Hit Release", "new"));
+  String statement(Customer customer) {
 
-        BigDecimal totalAmount = BigDecimal.valueOf(0);
-        int frequentRenterPoints = 0;
-        StringBuilder resultBuilder = new StringBuilder("Rental Record for " + customer.getName() + "\n");
-        for (MovieRental r : customer.getRentals()) {
-            Movie movie = movies.get(r.getMovieId());
-            BigDecimal thisAmount = BigDecimal.valueOf(0);
+    BigDecimal totalAmount = BigDecimal.valueOf(0);
+    int frequentRenterPoints = 0;
+    StringBuilder resultBuilder = new StringBuilder("Rental Record for " + customer.getName() + "\n");
 
-            // determine amount for each movie
-            switch (movie.getCode()) {
-                case "regular":
-                    thisAmount = BigDecimal.valueOf(2);
-                    if (r.getDays() > 2) {
-                        thisAmount = BigDecimal.valueOf((r.getDays() - 2) * 1.5).add(thisAmount);
-                    }
-                    break;
-                case "new":
-                    thisAmount = BigDecimal.valueOf(r.getDays() * 3);
-                    break;
-                case "childrens":
-                    thisAmount = BigDecimal.valueOf(1.5);
-                    if (r.getDays() > 3) {
-                        thisAmount = BigDecimal.valueOf((r.getDays() - 3) * 1.5).add(thisAmount);
-                    }
-                    break;
-            }
+    for (MovieRental rental : customer.getRentals()) {
+      Movie movie = movies.get(rental.getMovieId());
 
-            //add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (Objects.equals(movie.getCode(), "new") && r.getDays() > 2) frequentRenterPoints++;
+      // determine amount for each movie
+      BigDecimal thisAmount = determineRentalPriceForCategory(movie.getCode(), rental);
 
-            //print figures for this rental
-            resultBuilder
-                    .append("\t")
-                    .append(movie.getTitle())
-                    .append("\t")
-                    .append(thisAmount)
-                    .append("\n");
+      //add frequent renter points
+      frequentRenterPoints = getFrequentRenterPointsForRental(movie.getCode(), rental, frequentRenterPoints);
 
-            totalAmount = totalAmount.add(thisAmount);
-        }
-        String result = resultBuilder.toString();
-        // add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points\n";
+      //print figures for this rental
+      resultBuilder
+          .append("\t")
+          .append(movie.getTitle())
+          .append("\t")
+          .append(thisAmount)
+          .append("\n");
 
-        return result;
+      totalAmount = totalAmount.add(thisAmount);
     }
+
+    // add footer lines
+    resultBuilder
+        .append("Amount owed is ")
+        .append(totalAmount)
+        .append("\n")
+        .append("You earned ")
+        .append(frequentRenterPoints)
+        .append(" frequent renter points\n");
+
+    return resultBuilder.toString();
+  }
+
+  private int getFrequentRenterPointsForRental(String movieCode, MovieRental rental, int currentRenterPoints) {
+    if (Objects.equals(movieCode, "new") && rental.getDays() > 2) {
+      return currentRenterPoints + 2; // add bonus for a two day new release rental
+    } else {
+      return currentRenterPoints + 1;
+    }
+  }
+
+  private HashMap<String, Movie> getMovies() {
+    HashMap<String, Movie> movies = new HashMap<>();
+    movies.put("F001", new Movie("Ran", "regular"));
+    movies.put("F002", new Movie("Trois Couleurs: Bleu", "regular"));
+    movies.put("F003", new Movie("Cars 2", "childrens"));
+    movies.put("F004", new Movie("Latest Hit Release", "new"));
+
+    return movies;
+  }
+
+  private BigDecimal determineRentalPriceForCategory(String movieCode, MovieRental rental) {
+    BigDecimal rentalPriceForCategory = BigDecimal.valueOf(0);
+    switch (movieCode) {
+      case "regular":
+        rentalPriceForCategory = BigDecimal.valueOf(2);
+        if (rental.getDays() > 2) {
+          return BigDecimal.valueOf((rental.getDays() - 2) * 1.5)
+              .add(rentalPriceForCategory);
+        }
+      case "new":
+        return BigDecimal.valueOf(rental.getDays() * 3);
+      case "childrens":
+        rentalPriceForCategory = BigDecimal.valueOf(1.5);
+        if (rental.getDays() > 3) {
+          return BigDecimal.valueOf((rental.getDays() - 3) * 1.5)
+              .add(rentalPriceForCategory);
+        }
+        break;
+    }
+    return rentalPriceForCategory;
+  }
+
 }
--- test/com/greatersum/rental/RentalTests.java
+++ test/com/greatersum/rental/RentalTests.java
@@ -1,21 +1,30 @@
 package com.greatersum.rental;
 
-import org.approvaltests.Approvals;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 public class RentalTests {
-    @Test
-    public void MartinTest() {
-        Customer customer = new Customer(
-                "martin", Arrays.asList(
-                        new MovieRental("F001", 3),
-                        new MovieRental("F002", 1)
-        ));
-        RentalInfo info = new RentalInfo();
-        String actualResult = info.statement(customer);
-        Approvals.verify(actualResult);
-    }
+  @Test
+  public void MartinTest() {
+    Customer customer = new Customer(
+        "martin", Arrays.asList(
+        new MovieRental("F001", 3),
+        new MovieRental("F002", 1)
+    ));
+    RentalInfo info = new RentalInfo();
+    String actualResult = info.statement(customer);
+
+    String expectedResult =
+        "Rental Record for martin\n"
+        + "\tRan\t3.5\n"
+        + "\tTrois Couleurs: Bleu\t3\n"
+        + "Amount owed is 6.5\n"
+        + "You earned 2 frequent renter points\n";
+
+    Assert.assertEquals(expectedResult, actualResult);
+  }
 }
